cd01ecae808540d4fe82b7f6dd2c70d212cdaa0b
Javier Vargas <jabo94454@gmail.com>
2022-05-09 19:09:36 -0500

    Se arreglan los errores como tú :v

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const { Usuario, UsuarioSchema } = require("./usuario.model");
 const { Venta, VentaSchema } = require("./venta.model");
 const { DetalleVenta, DetalleVentaSchema } = require("./detalleVenta.model");
 const { Turno, TurnoSchema } = require("./turno.model");
-const { Repartidor, RepartidorSchema } = require("./repartidores.model");
-const { categorias, categoriasSchema } = require("./categorias.model");
-const { metodoPago, metodoPagoSchema } = require("./metodoPago.model");
-const { marcas, marcasSchema } = require("./marcas.model");
-const { imagenes, imagenesSchema } = require("./imagenes.model");
+const { Repartidor, RepartidorSchema } = require("./repartidor.model");
+const { Categoria, CategoriaSchema } = require("./categoria.model");
+const { MetodoPago, MetodoPagoSchema } = require("./metodoPago.model");
+const { Marca, MarcaSchema } = require("./marca.model");
+const { Imagen, ImagenSchema } = require("./imagen.model");
 const { Proveedor, ProveedorSchema } = require("./proveedor.model");
 
 function setupModels(sequelize) {
@@ -17,10 +17,10 @@ function setupModels(sequelize) {
   DetalleVenta.init(DetalleVentaSchema, DetalleVenta.config(sequelize));
   Turno.init(TurnoSchema, Turno.config(sequelize));
   Repartidor.init(RepartidorSchema, Repartidor.config(sequelize));
-  categorias.init(categoriasSchema, categorias.config(sequelize));
-  metodoPago.init(metodoPagoSchema, metodoPago.config(sequelize));
-  marcas.init(marcasSchema, marcas.config(sequelize));
-  imagenes.init(imagenesSchema, imagenes.config(sequelize));
+  Categoria.init(CategoriaSchema, Categoria.config(sequelize));
+  MetodoPago.init(MetodoPagoSchema, MetodoPago.config(sequelize));
+  Marca.init(MarcaSchema, Marca.config(sequelize));
+  Imagen.init(ImagenSchema, Imagen.config(sequelize));
   Proveedor.init(ProveedorSchema, Proveedor.config(sequelize));
 
   Venta.associate(sequelize.models);
