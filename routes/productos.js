b5e4237c6508e20659bffd430749dda4affe8e40
Javier Vargas <jabo94454@gmail.com>
2022-05-11 12:47:29 -0500

    Se modifica la respuesta de consultar a los productos

--- routes/productos.js
+++ routes/productos.js
@@ -1,44 +1,50 @@
-const express = require('express')
+const express = require("express");
 
-const ProductoService = require('../services/producto.service')
+const ProductoService = require("../services/producto.service");
 
 const productosApi = (app) => {
+  const router = express.Router();
 
-    const router = express.Router()
+  app.use("/api/productos", router);
 
-    app.use('/api/productos', router)
+  const productoService = new ProductoService();
 
-    const productoService = new ProductoService()
+  router.get("/", async (req, res, next) => {
+    try {
+      const productos = await productoService.findAll();
 
-    router.get('/', async(req, res, next) => {
-        
-        try {
-            const productos = await productoService.findAll()
-            
-            res.status(200).json({
-                data: productos,
-                message: 'productos listados'
-            })
-        } catch (error) { 
-            res.status(500).json({
-                data: error,
-                message: 'productos no listados'
-            })
-        }
-    })
+      res.status(200).json(
+        productos.map((msg) => {
+          return {
+            nombre: msg.nombre,
+            descripcion: msg.descripcion,
+            precio: msg.precio,
+          };
+        })
+      );
+      //   res.status(200).json({
+      //     data: productos,
+      //   });
+    } catch (error) {
+      res.status(500).json({
+        data: error,
+        message: "productos no listados",
+      });
+    }
+  });
 
-    router.post('/', async(req, res, next) => {
-        try {
-            const producto = req.body
+  router.post("/", async (req, res, next) => {
+    try {
+      const producto = req.body;
 
-            const nuevoProducto = await productoService.create(producto)
-        } catch (error) {
-            res.status(500).json({
-                data: error,
-                message: 'producto no creado'
-            })
-        }
-    })
-}
+      const nuevoProducto = await productoService.create(producto);
+    } catch (error) {
+      res.status(500).json({
+        data: error,
+        message: "producto no creado",
+      });
+    }
+  });
+};
 
-module.exports = productosApi
+module.exports = productosApi;
